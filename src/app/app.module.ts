1d994b31de4538ef74607ebf4e29f8888a7ca62a
acerklaus <55672506+acerklaus@users.noreply.github.com>
2021-11-11 16:53:37 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginScreenComponent } from './login-screen/login-screen.component';
+import { HomeScreenComponent } from './home-screen/home-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginScreenComponent,
+    HomeScreenComponent
   ],
   imports: [
     BrowserModule
